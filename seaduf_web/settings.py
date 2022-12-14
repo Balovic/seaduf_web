24a4528eb5447bbc1b2b75695e03ab4e3afc3dbb
Victor <Youngbaloviclife@gmail.com>
2022-12-14 19:02:11 +0100

    first

--- seaduf_web/settings.py
+++ seaduf_web/settings.py
@@ -158,8 +158,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     'seaduf_web/static',
 ]
