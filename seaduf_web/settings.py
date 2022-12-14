8016fec2bd9218db5798531934de6a5a4717ff70
Victor <Youngbaloviclife@gmail.com>
2022-12-14 18:46:57 +0100

    loginter

--- seaduf_web/settings.py
+++ seaduf_web/settings.py
@@ -147,12 +147,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
+# Internationalization
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'US/Pacific'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
