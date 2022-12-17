798d65081236d1445dbd82b01283349fbcaf92e3
Victor <Youngbaloviclife@gmail.com>
2022-12-17 18:39:12 +0100

    allowed host

--- seaduf_web/settings.py
+++ seaduf_web/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y6ezhe*p5&hx@h9d+dk2je8ueja94@zt)x9h9$uihxsmn1ck13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['seaduf-env.eba-3pdey3y5.us-west-2.elasticbeanstalk.com', "127.0.0.1"]
+ALLOWED_HOSTS = ["seaduf-env.eba-s62apgne.us-west-2.elasticbeanstalk.com", "127.0.0.1"]
 
 
 # Application definition
